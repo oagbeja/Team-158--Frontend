bffe03d859b3462b65e2fc0042df32f0628daa15
Agbeja Joshua Olugbenga <talktogbenga@gmail.com>
2020-10-10 22:00:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.static("public"));
 
-app.listen(PORT,()=> {
-    console.log(`App is listening on port ${PORT}`);
+app.listen(PORT,() => {
+    //console.log(`App is listening on port ${PORT}`);
 });
